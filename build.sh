a042b5fe6728210b206e1a08982bbaac719ebf44
myxi <myxi@envs.net>
2024-10-18 18:32:55 +0530

    feat: add ci

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+TARGETS=("x86_64-unknown-linux-gnu" "aarch64-unknown-linux-gnu" "x86_64-pc-windows-msvc" "x86_64-apple-darwin" "aarch64-apple-darwin")
+NAME="athe.bin"
+
+mkdir -p bin
+
+for target in "${TARGETS[@]}"; do
+    echo "deno compile --target=$target --allow-net --allow-run main.ts --output ./bin/$target-$NAME"
+done
+
